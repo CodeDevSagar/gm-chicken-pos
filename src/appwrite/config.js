4b42bdedde6bbaac37bc026c8fb3b7d2377bdf48
Sagar <sagarartist28@gmail.com>
2025-12-12 22:16:28 +0530

    Added Fixed config and .env

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -10,8 +10,8 @@ const client = new Client()
 // Your Database and Collection IDs
 export const AppwriteConfig = {
     databaseId: '691df7bc000445abd06a',
-    userCollectionId: 'users',
-    productCollectionId: 'products',
+    usersCollectionId: 'users',
+    productsCollectionId: 'products',
     salesCollectionId: 'sales',
     purchasesCollectionId: 'purchases',
     settingsCollectionId: 'settings',
